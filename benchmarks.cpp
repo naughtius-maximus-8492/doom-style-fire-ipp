3ba4655353043267c3367cca4497e24d6ced0730
naughtius-maximus-8492 <naughtius.maximus8492@gmail.com>
2025-12-06 12:45:16 +0000

    Add bencmark for getFrame

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -5,14 +5,18 @@
 
 TEST_CASE("Secret Entrance One")
 {
-    std::array<int, 10> sizes = {16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
+    std::array<int, 8> sizes = {64, 128, 256, 512, 1024, 2048, 4096, 8192};
     for (int size : sizes)
     {
+        const doomASCIIFire fire(size, size);
         BENCHMARK("Decay Step" + std::to_string(size) + "x" + std::to_string(size))
         {
-            const doomASCIIFire fire(size, size);
             fire.decayStep();
         };
 
+        BENCHMARK("Get frame" + std::to_string(size) + "x" + std::to_string(size))
+        {
+            std::string frame = fire.getFrame();
+        };
     }
 }
