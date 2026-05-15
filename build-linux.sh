fb035e418798bf0b9dbeb99e20f86b0475c4bc1d
naughtius-maximus-8492 <naughtius.maximus8492@gmail.com>
2026-05-15 18:46:56 +0100

    Preserve _deps

--- build-linux.sh
+++ build-linux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+BUILD_FOLDER=build/
+
 if [[ $1 == "clean" ]]; then
-  echo "Cleaning build directory..."
-  rm -rf build/
+  echo "Cleaning build directory ..."
+  cmake --build $BUILD_FOLDER --target clean_preserve-deps
 fi
 
-cmake -B build/ -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build build/ -j
+cmake -B $BUILD_FOLDER -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build $BUILD_FOLDER -j
