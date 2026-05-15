2ac86b7915e60436e4949ab7ca712a70b758fad6
naughtius-maximus-8492 <naughtius.maximus8492@gmail.com>
2026-05-15 18:19:43 +0100

    Move CPM.cmake to the right place

--- build-linux.sh
+++ build-linux.sh
@@ -2,8 +2,8 @@
 
 if [[ $1 == "clean" ]]; then
   echo "Cleaning build directory..."
-  rm -rf build-linux/
+  rm -rf build/
 fi
 
-cmake -B build-linux/ -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build build-linux/ -j
+cmake -B build/ -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build/ -j
