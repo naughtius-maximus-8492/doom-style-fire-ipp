7f27bf1a5add307b282046925a770f6ab61c9f46
naughtius-maximus-8492 <naughtius.maximus8492@gmail.com>
2026-05-15 18:13:51 +0100

    Integrate CPM

--- build-linux.sh
+++ build-linux.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "clean" ]]; then
   echo "Cleaning build directory..."
-  rm -rf build/
+  rm -rf build-linux/
 fi
 
 cmake -B build-linux/ -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icpx cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
